fead1824f32d2f0e912c26b4c1232f224f841829
Sankar boro <sankar.boro@yahoo.com>
2022-02-25 21:03:18 +0530

    b43fcff

---
+++ scripts/benchmarks.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+steps=50
+repeat=20
+
+statemineOutput=./polkadot-parachains/statemine/src/weights
+statemintOutput=./polkadot-parachains/statemint/src/weights
+westmintOutput=./polkadot-parachains/westmint/src/weights
+
+statemineChain=statemine-dev
+statemintChain=statemint-dev
+westmintChain=westmint-dev
+
+pallets=(
+    pallet_assets
+	pallet_balances
+	pallet_collator_selection
+	pallet_multisig
+	pallet_proxy
+	pallet_session
+	pallet_timestamp
+	pallet_utility
+    pallet_uniques
+    frame_system
+)
+
+for p in ${pallets[@]}
+do
+	./target/production/polkadot-collator benchmark \
+		--chain=$statemineChain \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--pallet=$p  \
+		--extrinsic='*' \
+		--steps=$steps  \
+		--repeat=$repeat \
+		--json \
+        --header=./file_header.txt \
+		--output=$statemineOutput
+
+	./target/production/polkadot-collator benchmark \
+		--chain=$statemintChain \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--pallet=$p  \
+		--extrinsic='*' \
+		--steps=$steps  \
+		--repeat=$repeat \
+		--json \
+        --header=./file_header.txt \
+		--output=$statemintOutput
+
+	./target/production/polkadot-collator benchmark \
+		--chain=$westmintChain \
+		--execution=wasm \
+		--wasm-execution=compiled \
+		--pallet=$p  \
+		--extrinsic='*' \
+		--steps=$steps  \
+		--repeat=$repeat \
+		--json \
+        --header=./file_header.txt \
+		--output=$westmintOutput
+done
